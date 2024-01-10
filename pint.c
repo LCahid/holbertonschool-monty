34f9e2db0376632310a83aa560abd2dedfceb775
root <gqasimova93@gmail.com>
2024-01-09 16:10:51 -0800

    prints the value at the top of the stack

--- pint.c
+++ pint.c
@@ -8,6 +8,7 @@ void pint_stack(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
+		free_opcode(), fclose(f), free(buf);
 		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
 		exit(EXIT_FAILURE);
 	}
